c9a24b1535f1fbdfd479f9f7337390cc8db59ae8
ITstart <ITstart@new.local.itstep>
2021-01-05 15:58:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let name = "";
+let password = "";
+function compare(){
+    if(name = "name"){
+        alert("Правильное имя");
+    }
+    else if(name != "name"){
+        alert("ошибка");
+    }
+    if(password = "password"){
+        alert("Правильный пароль");
+    }
+    else if(password != "password"){
+        alert("ошибка");
+    }
+
+}
+function message(){
+    let name = document.getElementById("name"/t).value;
+    let name = document.getElementById("password"/t).value;
+    document.getElementById("result").innerHTML = result
+    alert("Вы зарегистрировались")
+}
